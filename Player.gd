44bdec22c9a27b1eea44cdd93caf0b91fca4c1e9
Miha <stele1000@gmail.com>
2024-12-21 14:21:52 +0100

    Add audio

--- Player.gd
+++ Player.gd
@@ -1,8 +1,11 @@
 extends RigidBody3D
 
-
 @export_range(750.0, 3000.0) var thrust: float = 1000.0
 @export_range(60.0, 300.0) var torque_thrust: float = 100.0
+
+@onready var explosion_audio: AudioStreamPlayer = $ExplosionAudio
+@onready var victory_audio: AudioStreamPlayer = $VictoryAudio
+@onready var engine_thrust_audio: AudioStreamPlayer3D = $EngineThrust
 
 var is_transitioning: bool = false
 
@@ -10,6 +13,10 @@ var is_transitioning: bool = false
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
 		apply_central_force(basis.y * delta * thrust)
+		if !engine_thrust_audio.playing:
+			engine_thrust_audio.play()
+	else:
+		engine_thrust_audio.stop()
 	if Input.is_action_pressed("left"):
 		apply_torque(Vector3(0.0, 0.0, torque_thrust * delta))
 	if Input.is_action_pressed("right"):
@@ -31,17 +38,21 @@ func _on_body_entered(body: Node) -> void:
 
 func crash_sequence() -> void:
 	print("Game Lost")
+	explosion_audio.play()
 	is_transitioning = true
 	set_process(false)
+	engine_thrust_audio.stop()
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(3.0)
 	tween.tween_callback(get_tree().reload_current_scene)
 	
 	
 func complete_level(next_level_path: String) -> void:
 	print("Game won")
+	victory_audio.play()
+	is_transitioning = true
 	var tween = create_tween()
-	tween.tween_interval(1.0)
+	tween.tween_interval(3.0)
 	tween.tween_callback(
 		get_tree().change_scene_to_file.bind(next_level_path)
 		)
